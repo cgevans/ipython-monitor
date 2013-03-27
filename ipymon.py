ab0e655e5f7d3acef4f0e53d2e3ace62b0ecc520
Constantine Evans <cevans@evanslabs.org>
2013-03-27 14:35:54 -0700

    quite a few changes

--- ipymon.py
+++ ipymon.py
@@ -13,28 +13,50 @@ def main(ss):
     ss.addstr(1,1,"IPYTHON CLUSTER MONITOR")
     ss.hline(2,1,'-',30)
     ss.refresh()
-    ss.nodelay(True)
+    curses.halfdelay(5)
     
     while True:
-        ss.addstr(5,3,"# of engines: {}".format(len(rc.ids)))
-        ss.addstr(7,3,"Jobs: {} done / {} tasked / {} unassigned".format(*jobsrem()))
-        ss.addstr(9,3,"Done in last 1/5/10/60 minutes: {} / {} / {} / {}".format(*[doneintime(x) for x in [1,5,10,60]]))
-        #ss.addstr(11,3,"HI {}".format(ss.getkey()))
-        #lastcomp = mostrecent
-        ss.refresh()
-        time.sleep(5)
+        scrupdate(ss)
+        try: 
+            ss.refresh()
+            c = ss.getch()
+            if c == ord('q'):
+                sys.exit()
+            elif c == ord('p'):
+                purgemenu(ss)
+        except Exception,e:
+            raise e
 
-# def mostrecent(since):
-#   if since:
-#     recent = len(rc.db_query({'completed' : {'$gte' : since }}))
-#   else:
-#     recent = len(rc.db_query({'completed' : {'$ne' : None }}))
-#   
-  
+def purgemenu(ss):
+    ss.addstr(12,3,"Purge? ([r]esults, [e]verything, [c]ancel)")
+    ss.refresh()
+    q=True
+    while q:
+        scrupdate(ss)
+        ss.refresh()
+        c = ss.getch()
+        if c == ord('r'):
+            rc.purge_results('all')
+            q=False
+        elif c == ord('e'):
+            rc.purge_everything()
+            q=False
+        elif c == ord('c'):
+            q=False  
+    ss.addstr(12,2,"                                            ")
+
+def scrupdate(ss):
+    ss.addstr(4,3,"Time: {}               ".format(time.ctime()))    
+    ss.addstr(6,3,"# of engines: {}            ".format(len(rc.ids)))
+    ss.addstr(8,3,"Jobs: {} done / {} tasked / {} unassigned          ".format(*jobsrem()))
+    ss.addstr(10,3,"Done in last 1/5/10/60 minutes: {} / {} / {} / {}            ".format(*[doneintime(x) for x in [1,5,10,60]]))
+    ss.addstr(12,3,"[p]urge / [q]uit")
+    ss.refresh()
+   
   
 def doneintime(mins):
    since = datetime.now() - timedelta(1.0/24/60*mins)
-   nrecent = len(rc.db_query({'completed' : {'$gte' : since }}))
+   nrecent = len(rc.db_query({'completed' : {'$ne': None, '$gte' : since }}))
    return nrecent
   
 
@@ -44,8 +66,6 @@ def jobsrem():
    del(qs[u'unassigned'])
    t = sum( x[u'tasks'] for x in qs.values() )
    d = sum( x[u'completed'] for x in qs.values() )
-   #q = sum( x[u'queue'] for x in qs.values() )
-   #n = sum( x[u'tasks'] for x in qs.values() )
    return d,t,u
 
 curses.wrapper(main)
